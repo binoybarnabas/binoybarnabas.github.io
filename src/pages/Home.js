0e2a8aac46f7e6e5286864071fe462462d18fe55
binoy barnabas <binoybarnabas@gmail.com>
2022-08-09 12:08:44 +0530

    added counter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
+import {useState} from 'react';
 const Home = ()=>
 {
-    return (
 
+    const [changesValues,setChangedValue] = useState(1);
+    let changeValue = changesValues;
+    const changethevalue = () =>
+    {
+        
+        setChangedValue(++changeValue)
+        console.log(changesValues)
+        
+    }
+
+    return (
     <div className="Home">
         <center>
             <h1>enthada mone scene aana nee...</h1>
+            <h1>{changesValues}</h1>
+            <button className="butstyle" onClick={changethevalue}>click here</button>
         </center>
     </div>
     );
