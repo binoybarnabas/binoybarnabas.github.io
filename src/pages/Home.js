329b3d303f2e14d2f7ddff8333a720d97db08a18
binoy barnabas <binoybarnabas@gmail.com>
2022-08-09 11:20:49 +0530

    home page test

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+const Home = ()=>
+{
+    return (
+
+    <div className="Home">
+        <center>
+            <h1>enthada mone scene aana nee...</h1>
+        </center>
+    </div>
+    );
+}
+
+export default Home;
